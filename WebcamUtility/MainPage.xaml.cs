22a2ef10e3a89c8f9b0e350d5957eb101a1d7278
Reilly Wood <reilly.wood@icloud.com>
2020-05-02 17:34:32 -0700

    Add basic camera functionality

--- WebcamUtility/MainPage.xaml.cs
+++ WebcamUtility/MainPage.xaml.cs
@@ -17,14 +17,25 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
+using Windows.UI.Core;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.ApplicationModel;
+using System.Threading.Tasks;
+using Windows.System.Display;
+using Windows.Graphics.Display;
+
 namespace WebcamUtility
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        // TODO: automatically update when the list of webcams changes
+        // Next: animate the change 😎
+
         DeviceInformationCollection devices;
+        MediaCapture mediaCapture;
+        bool isPreviewing;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -36,7 +47,86 @@ namespace WebcamUtility
             devices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
             CameraListView.ItemsSource = devices;
             Debug.WriteLine($"{devices.Count} video devices found");
+            await StartPreviewAsync();
         }
+
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            await CleanupCameraAsync();
+        }
+
+        private async Task StartPreviewAsync()
+        {
+            try
+            {
+
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // This will be thrown if the user denied access to the camera in privacy settings
+                ShowMessageToUser("The app was denied access to the camera");
+                return;
+            }
+
+            try
+            {
+                PreviewControl.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+                isPreviewing = true;
+            }
+            catch (System.IO.FileLoadException)
+            {
+
+                mediaCapture.CaptureDeviceExclusiveControlStatusChanged += _mediaCapture_CaptureDeviceExclusiveControlStatusChanged;
+            }
+
+        }
+
+        private async void _mediaCapture_CaptureDeviceExclusiveControlStatusChanged(MediaCapture sender, MediaCaptureDeviceExclusiveControlStatusChangedEventArgs args)
+        {
+            if (args.Status == MediaCaptureDeviceExclusiveControlStatus.SharedReadOnlyAvailable)
+            {
+                ShowMessageToUser("The camera preview can't be displayed because another app has exclusive access");
+            }
+            else if (args.Status == MediaCaptureDeviceExclusiveControlStatus.ExclusiveControlAvailable && !isPreviewing)
+            {
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    await StartPreviewAsync();
+                });
+            }
+        }
+
+        private async Task CleanupCameraAsync()
+        {
+            if (mediaCapture != null)
+            {
+                if (isPreviewing)
+                {
+                    await mediaCapture.StopPreviewAsync();
+                }
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    PreviewControl.Source = null;
+                    mediaCapture.Dispose();
+                    mediaCapture = null;
+                });
+            }
+
+        }
+
+        private void ShowMessageToUser(string message)
+        {
+            //TODO: implement this
+            throw new NotImplementedException(message);
+        }
+
+
 
         // The default SplitView behaviour is annoying; the pane closes as soon as the window is resized
         // or the content is clicked. Suppress it.
@@ -44,9 +134,6 @@ namespace WebcamUtility
         {
             args.Cancel = true;
         }
-
-        // TODO: automatically update when the list of webcams changes
-        // Next: animate the change 😎
 
     }
 }
